1556f6c75454ec4524e251f03bce09a9b33ddc1a
AluruPavani <69709115+AluruPavani@users.noreply.github.com>
2021-02-24 15:49:18 +0530

    Update DataReader.java

--- DataReader.java
+++ DataReader.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
-public class DataReader {
+public class BankDataReader {
 
 	public static void main(String[] args)throws IOException {
 		List<String> bankAccount=Files.readAllLines(Paths.get("C:\\File\\bankaccount.txt"));
 		List<String> bankTransaction=Files.readAllLines(Paths.get("C:\\File\\banktransactions.txt"));
-		List<String> result=new ArrayList();
-		result.add("Name,Age,Gender,Bank,CardNumber,TransactionDate,Amount");
+		List<String> output=new ArrayList();
+		output.add("Name,Age,Gender,Bank,CardNumber,TransactionDate,Amount");
 		bankAccount.remove(0);
 		bankTransaction.remove(0);
 		for(String str:bankAccount) {
@@ -20,7 +20,7 @@ public class DataReader {
 			for(String str2:bankTransaction) {
 				String[] s1=str2.split(",");
 				if(Objects.equals(s[4],s1[0])) {
-					  result.add(s[0]+"\t"+s[1]+"\t"+s[2]+"\t"+s[3]+"\t\t"+s[4]+"\t\t"+s1[1]+"\t\t"+s1[2]);
+					  output.add(s[0]+"\t"+s[1]+"\t"+s[2]+"\t"+s[3]+"\t\t"+s[4]+"\t\t"+s1[1]+"\t\t"+s1[2]);
 					
 				}
 			}
@@ -31,7 +31,7 @@ public class DataReader {
 		System.out.println("Enter the name:");
 		string=scnr.nextLine();
 		Path path=Paths.get("C:\\File\\data.txt");
-		 System.out.println("Name"+"\t"+"Age"+"\t"+"Gender"+"\t"+"Bank"+"\t"+"CardNumber"+"\t"+"TransactionDate"+"\t"+"Amount");
+		 System.out.println("Name"+"\t"+"Age"+"\t"+"Gender"+"\t"+"Bank"+"\t\t"+"CardNumber"+"\t"+"TransactionDate"+"\t"+"Amount");
 		Files.lines(path)
 		.filter(str->str.contains(string))
 		.forEach(System.out::println);
